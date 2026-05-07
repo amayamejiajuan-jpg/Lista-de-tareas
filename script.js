98e7e2f8d7944957f26ee2f92b9573ccfaf6f39f
Juanp <amayamejiajuan@gmail.com>
2026-05-07 14:34:47 -0500

    segundo cambio

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const inputTarea = document.getElementById("inputTarea")
 const Boton = document.getElementById("btnAgregar")
 const contenedor = document.getElementById("contenedor")
 
+
 function agregar() {
     const valor = inputTarea.value
     const tarea = document.createElement("div")
     contenedor.appendChild(tarea)
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    tarea.appendChild(checkbox)
 
     const parrafo = document.createElement("p")
     tarea.appendChild(parrafo)
@@ -19,5 +23,6 @@ function agregar() {
         tarea.remove()
     }
     eliminar.addEventListener("click", eliminarTarea)
+
 }
 Boton.addEventListener("click", agregar)
