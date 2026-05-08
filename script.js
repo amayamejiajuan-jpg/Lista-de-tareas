72c1125d1c412d6cc24bd4f844f4ecda74f7dc1e
Juanp <amayamejiajuan@gmail.com>
2026-05-07 19:30:13 -0500

    ultimo cambio

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
 const inputTarea = document.getElementById("inputTarea")
 const Boton = document.getElementById("btnAgregar")
 const contenedor = document.getElementById("contenedor")
+const tareaParrafo = document.getElementById("tareas")
+const completadoParrafo = document.getElementById("completadas")
 
+let total = 0
+let completadas = 0
 
 function agregar() {
     const valor = inputTarea.value
-    const tarea = document.createElement("div")
-    contenedor.appendChild(tarea)
-    const checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
-    tarea.appendChild(checkbox)
 
-    const parrafo = document.createElement("p")
-    tarea.appendChild(parrafo)
-    parrafo.textContent = valor
+    if (valor.trim() == "") {
+        alert("Ingrese un valor")
+    } else {
+        const tarea = document.createElement("div")
+        contenedor.appendChild(tarea)
 
-    const eliminar = document.createElement("button")
-    tarea.appendChild(eliminar)
-    eliminar.textContent = "eliminar"
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        tarea.appendChild(checkbox)
 
-    function eliminarTarea() {
-        tarea.remove()
+        function tareaCompletada() {
+            if (checkbox.checked == true) {
+                completadas = completadas + 1
+            } else {
+                completadas = completadas - 1
+            }
+            completadoParrafo.textContent = "completadas: " + completadas
+        }
+
+        checkbox.addEventListener("change", tareaCompletada)
+
+        const parrafo = document.createElement("p")
+        tarea.appendChild(parrafo)
+        parrafo.textContent = valor
+
+        const eliminar = document.createElement("button")
+        tarea.appendChild(eliminar)
+        eliminar.className = "eliminar"
+        
+        const iconoEliminar = document.createElement("img")
+        eliminar.appendChild(iconoEliminar)
+        iconoEliminar.src = "./trash.png"
+
+        function eliminarTarea() {
+            tarea.remove()
+            total = total - 1
+            tareaParrafo.textContent = "tareas " + total
+        }
+        eliminar.addEventListener("click", eliminarTarea)
+
+        total = total + 1
+        tareaParrafo.textContent = "tareas " + total
+
+        inputTarea.value = ""
     }
-    eliminar.addEventListener("click", eliminarTarea)
+
 
 }
 Boton.addEventListener("click", agregar)
